e5ed0fcf05a3cdc004ea89eff3dc47e4bb51ba51
luisiacc <luiscorderoacc@gmail.com>
2023-10-02 23:08:11 -0600

    t4

--- remix.config.js
+++ remix.config.js
@@ -1,24 +1,12 @@
-import { flatRoutes } from 'remix-flat-routes'
-
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 export default {
 	cacheDirectory: './node_modules/.cache/remix',
-	ignoredRouteFiles: ['**/*'],
+	ignoredRouteFiles: ['**/.*'],
 	serverModuleFormat: 'esm',
 	serverPlatform: 'node',
 	tailwind: true,
 	postcss: true,
 	watchPaths: ['./tailwind.config.ts'],
-	routes: async defineRoutes => {
-		return flatRoutes('routes', defineRoutes, {
-			ignoredRouteFiles: [
-				'.*',
-				'**/*.css',
-				'**/*.test.{js,jsx,ts,tsx}',
-				'**/__*.*',
-			],
-		})
-	},
 }
--- server/index.ts
+++ server/index.ts
@@ -132,59 +132,6 @@ app.use(
 // When running tests or running in development, we want to effectively disable
 // rate limiting because playwright tests are very fast and we don't want to
 // have to wait for the rate limit to reset between tests.
-const maxMultiple =
-	MODE !== 'production' || process.env.PLAYWRIGHT_TEST_BASE_URL ? 10_000 : 1
-const rateLimitDefault = {
-	windowMs: 60 * 1000,
-	max: 1000 * maxMultiple,
-	standardHeaders: true,
-	legacyHeaders: false,
-	// Fly.io prevents spoofing of X-Forwarded-For
-	// so no need to validate the trustProxy config
-	validate: { trustProxy: false },
-}
-
-const strongestRateLimit = rateLimit({
-	...rateLimitDefault,
-	windowMs: 60 * 1000,
-	max: 10 * maxMultiple,
-})
-
-const strongRateLimit = rateLimit({
-	...rateLimitDefault,
-	windowMs: 60 * 1000,
-	max: 100 * maxMultiple,
-})
-
-const generalRateLimit = rateLimit(rateLimitDefault)
-app.use((req, res, next) => {
-	const strongPaths = [
-		'/login',
-		'/signup',
-		'/verify',
-		'/admin',
-		'/onboarding',
-		'/reset-password',
-		'/settings/profile',
-		'/resources/login',
-		'/resources/verify',
-	]
-	if (req.method !== 'GET' && req.method !== 'HEAD') {
-		if (strongPaths.some(p => req.path.includes(p))) {
-			return strongestRateLimit(req, res, next)
-		}
-		return strongRateLimit(req, res, next)
-	}
-
-	// the verify route is a special case because it's a GET route that
-	// can have a token in the query string
-	if (req.path.includes('/verify')) {
-		return strongestRateLimit(req, res, next)
-	}
-
-	return generalRateLimit(req, res, next)
-})
-
 function getRequestHandler(build: ServerBuild): RequestHandler {
 	function getLoadContext(_: any, res: any) {
 		return { cspNonce: "asd" }
